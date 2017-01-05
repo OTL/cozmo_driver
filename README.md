c5e6615ae0b296d7205b52fdfb4f5971a1dffa9b
Takashi Ogura <t.ogura@gmail.com>
2017-01-05 21:21:28 +0900

    Support joint state, led, tf...

--- README.md
+++ README.md
@@ -14,3 +14,27 @@ I don't know the true risk to do that.
 sudo apt-get install python3-yaml
 sudo pip3 install rospkg catkin_pkg
 ```
+
+### TODO
+
+* It is difficult to use tf2 on python3, it has tf2.so. How can we use?
+transform_stamped_to_tf2.py will convert the transform message to tf2 message.
+* camera stops after a few minutes.
+* use trajectory_msgs to command head angle and lift height.
+* publish battery state
+
+## Pub/Sub
+
+### Publish
+
+ * /image (sensor_msgs/Image) : camera image from cozmo. This is gray scale, but the format is rgb8.
+ * /joint_states (sensor_msgs/JointState) : This contains head angle and lift height
+ * /transforms (geometry_msgs/TransformStamped) : poses of visialbe cubes and cozmo
+
+### Subscribe
+
+ * /cmd_vel (geometry_msgs/Twist) : command velocity as usual. (velocity is not correct)
+ * /say (std_msgs/String) : cozmo says this text
+ * /head_angle (std_msgs/Float64) : command head angle [rad]
+ * /lift_height (std_msgs/Float64) : command lift height [m]
+ * /backpack_led (std_msgs/ColorRGBA) : led color on backpack
--- nodes/cozmo_driver.py
+++ nodes/cozmo_driver.py
@@ -5,11 +5,16 @@ import time
 
 import cozmo
 from cozmo.util import radians
+
 import rospy
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Transform
+from geometry_msgs.msg import TransformStamped
 from std_msgs.msg import String
 from std_msgs.msg import Float64
+from std_msgs.msg import ColorRGBA
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import JointState
 
 
 class CozmoRos(object):
@@ -19,26 +24,66 @@ class CozmoRos(object):
         self._cozmo = coz
         self._twist_sub = rospy.Subscriber('cmd_vel', Twist, self._twist_callback)
         self._say_sub = rospy.Subscriber('say', String, self._say_callback)
-        self._head_sub = rospy.Subscriber('head', Float64, self._move_head)
-        self._lift_sub = rospy.Subscriber('lift', Float64, self._move_lift)
-        self._image_pub = rospy.Publisher('image', Image, queue_size=1)
+        self._head_sub = rospy.Subscriber('head_angle', Float64, self._move_head)
+        self._lift_sub = rospy.Subscriber('lift_height', Float64, self._move_lift)
+        # TODO: better to use array (length=5)
+        self._backpack_led_sub = rospy.Subscriber(
+            'backpack_led', ColorRGBA, self._set_backpack_led)
+        self._image_pub = rospy.Publisher('image', Image, queue_size=100)
+        self._joint_state_pub = rospy.Publisher('joint_states', JointState,
+                                                queue_size=100)
+        self._tf_br = rospy.Publisher('transforms', TransformStamped,
+                                      queue_size=100)
 
     def _move_head(self, cmd):
-        action = self._cozmo.set_head_angle(radians(cmd.data), duration=0.1, in_parallel=True)
-#        action.wait_for_completed()
+        action = self._cozmo.set_head_angle(radians(cmd.data), duration=0.1,
+                                            in_parallel=True)
+        # action.wait_for_completed()
 
     def _move_lift(self, cmd):
-        action = self._cozmo.set_lift_height(height=cmd.data * 10, duration=0.1, in_parallel=True)
-#        action.wait_for_completed()
+        action = self._cozmo.set_lift_height(height=cmd.data * 10, duration=0.1,
+                                             in_parallel=True)
+        # action.wait_for_completed()
+
+    def _set_backpack_led(self, msg):
+        light = cozmo.lights.Light(cozmo.lights.Color(
+            rgba=[int(x * 255) for x in [msg.r, msg.g, msg.b, msg.a]])
+        self._cozmo.set_all_backpack_lights(light)
 
     def _twist_callback(self, cmd):
-        # TODO: convert m/s to motor speed
+        # TODO: convert m/s to motor speed actually
         lv = (cmd.linear.x + cmd.angular.z) * 100.0
         rv = (cmd.linear.x - cmd.angular.z) * 100.0
         self._cozmo.drive_wheels(lv, rv)
 
     def _say_callback(self, msg):
-        self._cozmo.say_text(msg.data).wait_for_completed()
+        self._cozmo.say_text(msg.data)
+
+    def _publish_objects(self):
+        def convert_pose_to_ros_msg(pose):
+            transform_msg = Transform()
+            transform_msg.translation.x = pose.position.x * 0.001
+            transform_msg.translation.y = pose.position.y * 0.001
+            transform_msg.translation.z = pose.position.z * 0.001
+            transform_msg.rotation.w = pose.rotation.q0
+            transform_msg.rotation.x = pose.rotation.q1
+            transform_msg.rotation.y = pose.rotation.q2
+            transform_msg.rotation.z = pose.rotation.q3
+            return transform_msg
+
+        def convert_pose_to_ros_msg(pose, name):
+            # TODO: use TransformStamped with tf2
+            transform_msg = TransformStamped()
+            transform_msg.header.stamp = rospy.Time.now()
+            transform_msg.header.frame_id = name
+            transform_msg.child_frame_id = 'map'
+            transform_msg.transform = convert_pose_to_ros_msg(pose)
+            return transform_msg
+
+        for obj in self._cozmo.world.visible_objects:
+            self._tf_br.publish(
+                convert_pose_to_ros_msg(obj.pose, 'cube_' + str(obj.object_id)))
+        self._tf_br.publish(convert_pose_to_ros_msg(self._cozmo.pose, 'cozmo'))
 
     def _publish_image(self):
         camera_image = self._cozmo.world.latest_image
@@ -52,14 +97,27 @@ class CozmoRos(object):
             ros_img.data = img.tobytes()
             ros_img.header.frame_id = 'cozmo_camera'
             cozmo_time = camera_image.image_recv_time
-            ros_img.header.stamp = rospy.Time.from_sec(cozmo_time)
-#            ros_img.header.stamp = rospy.Time.now()
+#            ros_img.header.stamp = rospy.Time.from_sec(cozmo_time)
+            ros_img.header.stamp = rospy.Time.now()
             self._image_pub.publish(ros_img)
+
+    def _publish_joint_state(self):
+        js = JointState()
+        js.header.stamp = rospy.Time.now()
+        js.header.frame_id = 'cozmo'
+        js.name = ['head', 'lift']
+        js.position = [self._cozmo.head_angle.radians,
+                       self._cozmo.lift_height.distance_mm * 0.001]
+        js.velocity = [0.0, 0.0]
+        js.effort = [0.0, 0.0]
+        self._joint_state_pub.publish(js)
 
     def spin(self):
         r = rospy.Rate(10)
         while not rospy.is_shutdown():
             self._publish_image()
+            self._publish_objects()
+            self._publish_joint_state()
             r.sleep()
         self._cozmo.stop_all_motors()
 
